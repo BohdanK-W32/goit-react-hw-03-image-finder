62b18e4b964cabdd712ed97768e11727d3406043
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2019-07-22 23:12:26 +0300

    Updates

---
+++ precache-manifest.519f07c9518be027027343da0a50e4e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926a9e4271727ba423288c4cdc6923ea",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e3dc2fe9acd5e85c37b2",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6d1f1d22.chunk.css"
+  },
+  {
+    "revision": "63a7cf3304cb1b2c50ff",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.ad9f6583.chunk.js"
+  },
+  {
+    "revision": "e3dc2fe9acd5e85c37b2",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f4202782.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
